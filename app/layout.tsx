e6ba136f5837d4e6374d535dfe2348aab057a4f2
Elias Sjödin <elias@jamee.se>
2024-01-02 09:08:48 +0100

    test with only notification provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,9 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
 	<NotificationProvider>
-	  <ToastProvider>
-	    Hello
-	  </ToastProvider>
+	  Hello
 	</NotificationProvider>
       </body>
     </html>
