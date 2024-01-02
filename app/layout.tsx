4d9a8d45f40f4459eebcc069c78213025aac8afe
Elias Sjödin <elias@jamee.se>
2024-01-02 09:03:15 +0100

    test to remove everything from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,11 +17,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-	<NotificationProvider>
-	  <ToastProvider>
-	    Hello
-	  </ToastProvider>
-	</NotificationProvider>
       </body>
     </html>
   )
